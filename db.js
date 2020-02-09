0aba4373581575b79fe0995a71a8cc69607eeec6
Jaco <jacowally8@gmail.com>
2020-02-09 12:26:02 -0300

    Segundo Commit

--- db.js
+++ db.js
@@ -1,17 +1,70 @@
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/testeProjetei');
 
-var userSchema = new mongoose.Schema({
-    nome: String
-    // telefone: String, 
-    // data_nasc: Date,
-    // rg: String,
-    // cpf: String,
-    // endereco: String,
-    // cidade: String,
-    // estado: String,
-    // cep: String
-}, { collection: 'usercollection' }
-);
- 
-module.exports = { Mongoose: mongoose, UserSchema: userSchema }
+const mongoose = require("mongoose")
+
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost:27017/testeProjetei", {
+    seNewUrlParser: true
+}).then(() => {
+    console.log("MongoDB conectado...")
+}).catch((err) => {
+    console.log("Houve um erro ao se conectar ao mongoDB: "+err)
+})
+
+const UsuarioSchema = mongoose.Schema({
+
+    nome:{
+        type: String,
+        // para ser obrigatorio
+        require: true
+    },
+    telefone:{
+        type: Number,
+        require: true
+    },
+    data_nasc:{
+        type: Date,
+        require: true
+    },
+    rg:{
+        type: Number,
+        require: true
+    },
+    cpf:{
+        type: Number,
+        require: true
+    },
+    endereco:{
+        type: String,
+        require: true
+    },
+    cidade:{
+        type: String,
+        require: true
+    },
+    estado:{
+        type: String,
+        require: true
+    },
+    cep:{
+        type: Number,
+        require: true
+    },
+})
+
+mongoose.model('usuarios', UsuarioSchema )
+
+new UsuarioSchema ({
+    nome: "Leo",
+    telefone: 88773355,
+    data_nasc: "04-04-1992",
+    rg: 57789362,
+    cpf: 88877755533,
+    endereco: "Avenida",
+    cidade: "Pelotas",
+    estado: "RS",
+    cep: 97266000
+}).save().then(() => {
+    console.log("Usuario criado")
+}).catch((err) => {
+    console.log("Erro: " +err)
+})
